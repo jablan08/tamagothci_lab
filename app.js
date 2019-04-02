2bb1bbcc5aa88bdfc1de2ec9fe879f3ba46b3c33
Jablan <joshuaablan@gmail.com>
2019-04-02 12:56:25 -0700

    cleaned up code and removed comments

--- app.js
+++ app.js
@@ -34,7 +34,6 @@ class Tomagotchi {
 const pet = new Tomagotchi(`${prompt("Name your pet!")}`);
 const body = document.querySelector("body");
 const img = document.querySelector("img");
-// body.addNewElement(`<h1>Name:${pet.name}</h1>`)
 const name = document.querySelector(".name");
 const age = document.querySelector(".age");
 const hungry = document.querySelector(".hunger");
@@ -46,20 +45,15 @@ const sleepButton = document.querySelector(".goSleep");
 const shakeButton = document.querySelector(".shake");
 const checkers = document.querySelector(".checkers");
 const circle = document.querySelector("#circle");
+
 age.innerText += pet.age;
 name.innerText += pet.name;
 hungry.innerText += pet.hunger;
 bored.innerText += pet.boredom;
 sleepy.innerText += pet.sleepiness;
 
-
-// feed
 feedButton.addEventListener("click", pet.feed);
-
-// play
 playButton.addEventListener("click", pet.play);
-
-// sleep
 sleepButton.addEventListener("click", pet.sleep);
 shakeButton.addEventListener("click", pet.shake);
 
@@ -74,31 +68,24 @@ const render = () => {
 }
 let checkUp = setInterval(render, 120000)
 
-
 const oldAF = () => {
     pet.age += 1
     age.innerText = `Age: ${pet.age}`
     if (pet.age === 5) {
         img.src = "https://media.giphy.com/media/3KXUztaYAzHaFH6et3/giphy.gif";
-       
     } else if (pet.age === 10) {
-
         img.src = "https://media.giphy.com/media/7A4GONnjiqxmRhjfgH/giphy.gif";
       
     }
 }
-let ageUp = setInterval(oldAF, 5000)
+let ageUp = setInterval(oldAF, 60000)
 
-
-// DED AF
 const deadAF = () => {
     if (pet.sleepiness === 10 || pet.boredom === 10 || pet.hunger === 10) {
         clearInterval(checkUp);
         clearInterval(ageUp);
     checkers.style.color = "red";
-
     img.src = "gifs/dead.gif";
-
     alert("WASTED!! YOUR PET DIED!")
     }
 }
